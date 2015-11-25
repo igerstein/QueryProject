f961c9a204be43ed93b0d08cbafc6518356f8c5d
igerstein <isaac.gerstein1@gmail.com>
2015-11-25 11:37:18 -0500

    Restructured code and made separate methods for who and when

--- search.py
+++ search.py
@@ -2,13 +2,30 @@ import urllib2, google, bs4, re
 
 def getAnswer(query):
     """
-    Finds the answer to a qiven question.
+    Finds the answer to a given question.
 
     Args:
         query: A string representing the question to answer.
 
     Returns:
         The answer to the question as a string.
+    """
+    if query[:4].lower() == "who ":
+        return findWho(query)
+    elif query[:5].lower() == "when ":
+        return findWhen(query)
+    else:
+        return "Unable to find answer"
+
+def getRaw(query):
+    """
+    Gets the raw text from webpages that answer a question.
+
+    Args:
+        query: A string representing the question to answer.
+
+    Returns:
+        The text from webpages that answer the question as a string.
     """
     r = google.search(query, num = 10, start = 0, stop = 10)
     raw = ""
@@ -21,3 +38,29 @@ def getAnswer(query):
         except:
             pass
     return raw
+
+def findWho(query):
+    """
+    Finds the answer to a given "who" question.
+
+    Args:
+        query: A string representing the question to answer.
+
+    Returns:
+        The answer to the question as a string.
+    """
+    raw = getRaw(query)
+    return ""
+
+def findWhen(query):
+    """
+    Finds the answer to a given "when" question.
+
+    Args:
+        query: A string representing the question to answer.
+
+    Returns:
+        The answer to the question as a string.
+    """
+    raw = getRaw(query)
+    return ""
