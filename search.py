d74f578b69ea7b6f344d2283499f2c45be6a08f0
igerstein <isaac.gerstein1@gmail.com>
2015-11-29 11:17:23 -0500

    Answers who queries

--- search.py
+++ search.py
@@ -48,11 +48,11 @@ def findWho(query):
 
     Returns:
         The answer to the question as a string.
+
     """
     raw = getRaw(query)
-    names = re.findall("[A-Z][a-z]+ [A-Z][a-z]+", raw)
-    print names
-    return ""
+    names = getNames(raw)
+    return mostCommon(names)
 
 def findWhen(query):
     """
@@ -66,3 +66,56 @@ def findWhen(query):
     """
     raw = getRaw(query)
     return ""
+
+def getNames(text):
+    """
+    Gets a list of names from a block of text.
+
+    Args:
+        text: The text to find the names from as a string.
+
+    Returns:
+        A list of names.
+
+    >>> getNames("John Smith something John John Smith")
+    ['John Smith', 'John John Smith']
+    >>> getNames("Mr. John Smith something Mr.John Smith something Mrs. John Smith")
+    ['Mr. John Smith', 'John Smith', 'Mrs. John Smith']
+    >>> getNames("John Smith-smith something John Smith-Smith")
+    ['John Smith', 'John Smith-Smith']
+    >>> getNames("A. B. Smith something John A. Smith something John A. B. Smith")
+    ['A. B. Smith', 'John A. Smith', 'John A. B. Smith']
+    """
+    exp = "(((Dr.|Mr.|Mrs.|Ms.) )?((([A-Z][a-z]+ )+([A-Z]. )*)|([A-Z]. )+)[A-Z][a-z]+(\-[A-Z][a-z]+)?)"
+    result = re.findall(exp, text)
+    names = []
+    for r in result:
+        names.append(r[0])
+    return names
+
+def mostCommon(L):
+    """
+    Returns the most common element in a list.
+
+    Args:
+        L: The list.
+
+    Returns:
+        The most common element.
+    """
+    count = {}
+    maxCount = 0
+    maxItem = None
+    for item in L:
+        if count.has_key(item):
+            count[item] += 1
+        else:
+            count[item] = 1
+        if count[item] > maxCount:
+            maxCount = count[item]
+            maxItem = item
+    return maxItem
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
